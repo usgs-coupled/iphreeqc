d40e573da934de78b83844b8e48f2e18ac7073c8
Scott R Charlton <charlton@usgs.gov>
2010-05-06 19:09:26 +0000

    added return vals for fortran
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqc/trunk@4353 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- tests/test_f.f
+++ tests/test_f.f
@@ -3,84 +3,98 @@
       IMPLICIT NONE
       INCLUDE 'IPhreeqc.f.inc'
       INTEGER(KIND=4) id
+
+      INTEGER(KIND=4) EXIT_SUCCESS
+      PARAMETER (EXIT_SUCCESS=0)
+
+      INTEGER(KIND=4) EXIT_FAILURE
+      PARAMETER (EXIT_FAILURE=1)
+
       INTEGER(KIND=4) F_MAIN
       
       id = CreateIPhreeqc()
       IF (id.LT.0) THEN
-        F_MAIN = 1
-        RETURN
+         F_MAIN = EXIT_FAILURE
+         RETURN
       ENDIF
       
       IF (GetOutputOn(id)) THEN
-        F_MAIN = 2
-        RETURN
+         F_MAIN = EXIT_FAILURE
+         RETURN
       ENDIF
-
+      
       IF (SetOutputOn(id,.TRUE.).NE.IPQ_OK) THEN
-        F_MAIN = 2
-        RETURN
+         F_MAIN = EXIT_FAILURE
+         RETURN
       ENDIF
-
+      
       IF (.NOT.GetOutputOn(id)) THEN
-        F_MAIN = 2
-        RETURN
+         F_MAIN = EXIT_FAILURE
+         RETURN
       ENDIF
-
+      
       IF (LoadDatabase(id, "phreeqc.dat").NE.0) THEN
-        CALL OutputError(id)
-        F_MAIN = 3
-        RETURN
+         CALL OutputError(id)
+         F_MAIN = EXIT_FAILURE
+         RETURN
       ENDIF
       
       IF (RunFile(id, "ex1").NE.0) THEN
-        CALL OutputError(id)
-        F_MAIN = 3
-        RETURN
+         CALL OutputError(id)
+         F_MAIN = EXIT_FAILURE
+         RETURN
       ENDIF
       
       IF (DestroyIPhreeqc(id).NE.0) THEN
-        CALL OutputError(id)
-        F_MAIN = 5
-        RETURN
+         CALL OutputError(id)
+         F_MAIN = EXIT_FAILURE
+         RETURN
       ENDIF
       
-      F_MAIN = 0
+      F_MAIN = EXIT_SUCCESS
       RETURN
       
       END FUNCTION F_MAIN
-
-
+      
+      
       FUNCTION TestGetSet(id,getFunc,setFunc)
       
       IMPLICIT NONE
       INCLUDE 'IPhreeqc.f90.inc'
       INTEGER(KIND=4) id
       INTEGER(KIND=4) TESTGETSET
-
+      
       LOGICAL(KIND=4) getFunc
       INTEGER(KIND=4) setFunc
       
+      INTEGER(KIND=4) EXIT_SUCCESS
+      PARAMETER (EXIT_SUCCESS=0)
+      
+      INTEGER(KIND=4) EXIT_FAILURE
+      PARAMETER (EXIT_FAILURE=1)
+      
+      
       IF (getFunc(id)) THEN
-         TestGetSet = 2
+         TestGetSet = EXIT_FAILURE
          RETURN
       ENDIF
       
       IF (setFunc(id,.TRUE.).NE.IPQ_OK) THEN
-         TestGetSet = 2
+         TestGetSet = EXIT_FAILURE
          RETURN
       ENDIF
       
       IF (.NOT.getFunc(id)) THEN
-         TestGetSet = 2
+         TestGetSet = EXIT_FAILURE
          RETURN
       ENDIF
       
       IF (setFunc(id,.FALSE.).NE.IPQ_OK) THEN
-         TestGetSet = 2
+         TestGetSet = EXIT_FAILURE
          RETURN
       ENDIF
       
-      TestGetSet = 0  
+      TestGetSet = EXIT_SUCCESS
       RETURN
       
       END FUNCTION TestGetSet
--- tests/test_f90.f90
+++ tests/test_f90.f90
@@ -6,67 +6,70 @@ FUNCTION F_MAIN()
   INTEGER(KIND=4) F_MAIN
   INTEGER(KIND=4) TestGetSet
   
+  INTEGER(KIND=4),PARAMETER :: EXIT_SUCCESS = 0
+  INTEGER(KIND=4),PARAMETER :: EXIT_FAILURE = 1
+
   id = CreateIPhreeqc()
   IF (id.LT.0) THEN
-     F_MAIN = 1
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   ! Dump
   IF (TestGetSet(id,GetDumpOn,SetDumpOn).NE.0) THEN
-     F_MAIN = 2
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   ! Dump string
   IF (TestGetSet(id,GetDumpStringOn,SetDumpStringOn).NE.0) THEN
-     F_MAIN = 2
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   ! Error
   IF (TestGetSet(id,GetErrorOn,SetErrorOn).NE.0) THEN
-     F_MAIN = 2
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   ! Log
   IF (TestGetSet(id,GetLogOn,SetLogOn).NE.0) THEN
-     F_MAIN = 2
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   ! Output
   IF (TestGetSet(id,GetOutputOn,SetOutputOn).NE.0) THEN
-     F_MAIN = 2
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   ! Selected output
   IF (TestGetSet(id,GetSelectedOutputOn,SetSelectedOutputOn).NE.0) THEN
-     F_MAIN = 2
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   IF (LoadDatabase(id, "phreeqc.dat").NE.0) THEN
      CALL OutputError(id)
-     F_MAIN = 3
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   IF (RunFile(id, "ex1").NE.0) THEN
      CALL OutputError(id)
-     F_MAIN = 4
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
   IF (DestroyIPhreeqc(id).NE.0) THEN
      CALL OutputError(id)
-     F_MAIN = 5
+     F_MAIN = EXIT_FAILURE
      RETURN
   ENDIF
   
-  F_MAIN = 0
+  F_MAIN = EXIT_SUCCESS
   RETURN
   
 END FUNCTION F_MAIN
@@ -91,28 +94,30 @@ FUNCTION TestGetSet(id,getFunc,setFunc)
        INTEGER(KIND=4) setFunc
      END FUNCTION setFunc
   END INTERFACE
+  INTEGER(KIND=4),PARAMETER :: EXIT_SUCCESS = 0
+  INTEGER(KIND=4),PARAMETER :: EXIT_FAILURE = 1
   
   IF (getFunc(id)) THEN
-     TestGetSet = 2
+     TestGetSet = EXIT_FAILURE
      RETURN
   ENDIF
   
   IF (setFunc(id,.TRUE.).NE.IPQ_OK) THEN
-     TestGetSet = 2
+     TestGetSet = EXIT_FAILURE
      RETURN
   ENDIF
   
   IF (.NOT.getFunc(id)) THEN
-     TestGetSet = 2
+     TestGetSet = EXIT_FAILURE
      RETURN
   ENDIF
   
   IF (setFunc(id,.FALSE.).NE.IPQ_OK) THEN
-     TestGetSet = 2
+     TestGetSet = EXIT_FAILURE
      RETURN
   ENDIF
   
-  TestGetSet = 0  
+  TestGetSet = EXIT_SUCCESS
   RETURN
   
 END FUNCTION TestGetSet
