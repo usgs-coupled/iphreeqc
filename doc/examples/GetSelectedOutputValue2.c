9952bb955f9b45f64f1ecdecbbbb6d97a9ac3c8e
Scott R Charlton <charlton@usgs.gov>
2012-07-06 23:31:38 +0000

    finished GetSeletectedOutputValue2
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqc/trunk@6734 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- doc/examples/GetSelectedOutputValue2.c
+++ doc/examples/GetSelectedOutputValue2.c
@@ -7,47 +7,47 @@ const int SVALUE_LENGTH = 100;
 int main(void)
 {
   int id, i, j, vtype;
-  double dvalue
+  double dvalue;
   char svalue[SVALUE_LENGTH];
-
+  
   id = CreateIPhreeqc();
   if (id < 0) {
     return EXIT_FAILURE;
   }
-
+  
   if (LoadDatabase(id, "phreeqc.dat") != 0) {
     OutputErrorString(id);
     return EXIT_FAILURE;
   }
-
+  
   if (RunFile(id, "ex2") != 0) {
     OutputErrorString(id);
     return EXIT_FAILURE;
   }
-
+  
   printf("selected-output:\n");
   for (i = 0; i < GetSelectedOutputRowCount(id); ++i) {
     for (j = 0; j < GetSelectedOutputColumnCount(id); ++j) {
-      if (GetSelectedOutputValue2(id, i, j, vtype, dvalue, svalue, SVALUE_LENGTH) == VR_OK) {
-        switch (v.type) {
+      if (GetSelectedOutputValue2(id, i, j, &vtype, &dvalue, svalue, SVALUE_LENGTH) == VR_OK) {
+        switch (vtype) {
         case TT_LONG:
-          printf("%ld ", v.lVal);
+          printf("%ld ", (long)dvalue);
           break;
         case TT_DOUBLE:
-          printf("%g ", v.dVal);
+          printf("%g ", dvalue);
           break;
         case TT_STRING:
-          printf("%s ", v.sVal);
+          printf("%s ", svalue);
           break;
         }
       }
     }
     printf("\n");
   }
-
+  
   if (DestroyIPhreeqc(id)) {
     return EXIT_FAILURE;
   }
-
+  
   return EXIT_SUCCESS;
 }
