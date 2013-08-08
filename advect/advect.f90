3ae99a48775b992d54c27ee8ac2043b2ab8ba717
David L Parkhurst <dlpark@usgs.gov>
2013-08-08 17:23:03 +0000

    Modified to use callback
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc/trunk@7892 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- advect/advect.f90
+++ advect/advect.f90
@@ -1,19 +1,20 @@
 module Subs
-  integer    (kind=4), dimension(7) :: vt
-  real       (kind=8), dimension(7) :: dv
-  character (len=100), dimension(7) :: sv 
   integer                           :: Id
+  integer    (kind=4), dimension(8) :: vt
+  real       (kind=8), dimension(8) :: dv
+  character (len=100), dimension(8) :: sv 
   contains
   
   subroutine ExtractWrite(cell)
     include "IPhreeqc.f90.inc"
     integer    (kind=4), intent(in) :: cell
-    do j = 1, 7
+    do j = 1, 8
       ! Headings are on row 0
       Ierr = GetSelectedOutputValue(Id,1,j,vt(j),dv(j),sv(j))
       if(Ierr .ne. IPQ_OK) call EHandler()
     enddo
-    write(*,"(a,i2/2(5x,a,f7.2))") "Cell",cell,"pH:",dv(6),"SR(calcite):",dv(7) 
+    write(*,"(a,i2,a,i4,a/2(5x,a,f7.2))") "Cell",cell," ",int(dv(8))," ", &
+        "pH:",dv(6),"SR(calcite):",dv(7) 
   end subroutine ExtractWrite
   
   subroutine EHandler()
@@ -22,14 +23,49 @@ module Subs
     stop
   end subroutine EHandler    
 end module Subs
+    
+module MyData
+    double precision year
+end module MyData    
+
+module Callback    
+    contains
+    double precision function MyCallback(x1, x2, str)
+    use MyData
+!       
+!   Use of a callback is optional.
+!   
+!   The callback provides a way to obtain data from a Basic program
+!   through the variables x1, x2, and str, and send data to a 
+!   Basic program through the return value of the callback.
+!   
+!   The callback function is called whenever CALLBACK(x1, x2, str$)  
+!   is used in a Basic program (usually USER_PUNCH). See file "ic".
+!   
+        double precision, intent(in) :: x1, x2
+        character(*), intent(in)     :: str
+        MyCallback = -1.0
+	    ! An example of a datum available in main program
+        if (trim(str) .eq. "Year") then
+            write (*,"(/a,i2,a,f8.2)") "Callback for cell ", int(x1), ": pH ", x2
+            MyCallback = year
+        endif
+        return
+    end function MyCallback
+end module Callback
+    
 program Advect
   use Subs
+  use Callback
+  use MyData
   include "IPhreeqc.f90.inc"
   character(len=1024) Istring 
   
 !Create module, load database, define initial conditions and selected output
+  year = 2012.
   Id = CreateIPhreeqc()
   if (LoadDatabase(Id, "phreeqc.dat") .ne. 0) call EHandler()
+  if (SetBasicFortranCallback(id, MyCallback) .ne. 0) call EHandler()
   If (RunFile(Id, "ic") .ne. 0) call EHandler()
 
 !Run cell 1, extract/write result
@@ -45,6 +81,7 @@ program Advect
   Ierr = AccumulateLine(Id, "      C     " // sv(4))
   Ierr = AccumulateLine(Id, "      Ca    " // sv(5))
   Ierr = AccumulateLine(Id, "RUN_CELLS; -cells; 2; END")
+  year = year + 1
   if (RunAccumulated(Id) .ne. 0) call EHandler()
   call ExtractWrite(2)
   
