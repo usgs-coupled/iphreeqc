5c52b2744fddefce6387fae5f1d806d12ee68739
Scott R Charlton <charlton@usgs.gov>
2013-11-02 01:09:12 +0000

    updated f90 version
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqc/trunk@8158 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- doc/examples/F90GetVersionString.f90
+++ doc/examples/F90GetVersionString.f90
@@ -2,6 +2,7 @@ PROGRAM example
   INCLUDE "IPhreeqc.f90.inc"
   CHARACTER(LEN=80) :: version
 
+  WRITE(*,*) 'Version:'
   CALL GetVersionString(version)
   WRITE(*,*) TRIM(version)
 END PROGRAM example
