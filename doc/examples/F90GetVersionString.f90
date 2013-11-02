e5dcf9b9b0681eb36e3bd157fac150635f337350
Scott R Charlton <charlton@usgs.gov>
2013-11-02 01:40:47 +0000

    updated toc for getversionstring; changed single apostrophe to double for fortran example.
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqc/trunk@8162 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- doc/examples/F90GetVersionString.f90
+++ doc/examples/F90GetVersionString.f90
@@ -2,7 +2,7 @@ PROGRAM example
   INCLUDE "IPhreeqc.f90.inc"
   CHARACTER(LEN=80) :: version
 
-  WRITE(*,*) 'Version:'
+  WRITE(*,*) "Version:"
   CALL GetVersionString(version)
   WRITE(*,*) TRIM(version)
 END PROGRAM example
