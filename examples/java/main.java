2107220f6fb90eb71407b81bb0aae51051cd0965
Scott R Charlton <charlton@usgs.gov>
2015-09-09 21:53:35 +0000

    added java example
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqc/branches/swig2@10228 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ examples/java/main.java
@@ -0,0 +1,24 @@
+public class main {
+	public static void main(String argv[]) {
+		System.loadLibrary("swig_iphreeqc_java");
+		IPhreeqc i = new IPhreeqc();
+
+		if (i.LoadDatabase("phreeqc.dat") != 0) {
+			System.out.println(i.GetErrorString());
+			System.exit(1);
+		}
+
+		i.SetOutputStringOn(true);
+
+		java.lang.StringBuffer input = new java.lang.StringBuffer();
+		input.append("SOLUTION 1 Pure water \n");
+		input.append("EQUILIBRIUM_PHASES 1  \n");
+		input.append("    Calcite 0 10      \n");
+
+		if (i.RunString(input.toString()) == 0) {
+			System.out.println(i.GetOutputString());
+		} else {
+			System.out.println(i.GetErrorString());
+		}
+	}
+}
