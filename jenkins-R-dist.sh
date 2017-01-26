08cb9015de5ccf2d29c2b865152d5708a63a575d
Scott R Charlton <charlton@usgs.gov>
2017-01-26 02:02:44 +0000

    changed url
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqc/trunk@12283 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- jenkins-R-dist.sh
+++ jenkins-R-dist.sh
@@ -6,7 +6,7 @@ fi
 DATE=$(date -d $DATE "+%Y-%m-%d")
 # set VER
 if [ -z "$VER" ]; then
- wget https://cran.r-project.org/web/packages/phreeqc/ -O index.xhtml 2> /dev/null
+ wget https://cran.r-project.org/package=phreeqc -O index.xhtml 2> /dev/null
  VER=`xmlstarlet sel -t -m "/_:html/_:body/_:table[1]/_:tr[1]/_:td[2]" -v . -n index.xhtml 2> /dev/null`
  ver_major=`echo $VER | cut -d '.' -f 1`
  ver_minor=`echo $VER | cut -d '.' -f 2`
